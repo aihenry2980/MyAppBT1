5ac74129a774006bc9d8f57711bc7ea18a4d471e
JinXin <aihenry2980@gmail.com>
2022-12-29 13:52:39 +0900

    added youtube src url

--- app/src/main/java/com/jx/myappbt/MainActivity.kt
+++ app/src/main/java/com/jx/myappbt/MainActivity.kt
@@ -2,10 +2,14 @@ package com.jx.myappbt
 
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView;
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
 
@@ -44,6 +48,24 @@ class MainActivity : AppCompatActivity() {
 		{
 			btIv.setImageResource(R.drawable.ic_bt_off)
 		}
+
+		val btnTurnOn:Button = findViewById(R.id.turnOnBtn)
+		val btnTurnOff:Button = findViewById(R.id.turnOffBtn)
+		val btnDisc:Button = findViewById(R.id.discovBtn)
+
+		btnTurnOn.setOnClickListener{
+			if (btAdpt.isEnabled)
+			{
+				Toast.makeText(this, "Already On!", Toast.LENGTH_LONG).show()
+			}
+			else
+			{
+				var intent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+				ActivityResultContracts.StartActivityForResult
+			}
+		}
+		btnTurnOff.setOnClickListener{}
+		btnDisc.setOnClickListener{}
 
 	}
 }
---
+++ readme.md
@@ -0,0 +1,3 @@
+
+Src:
+https://www.youtube.com/watch?v=PtN6UTIu7yw&t=121s&ab_channel=AtifPervaiz
